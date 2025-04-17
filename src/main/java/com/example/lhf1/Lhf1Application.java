39366ff52e209a3f22fe3a7e364579dec8b7c8b8
sudhanshu2070 <sssallen2016@gmail.com>
2025-04-18 03:43:44 +0530

    Adding controllers 0.1

---
+++ src/main/java/com/example/lhf1/Lhf1Application.java
@@ -0,0 +1,13 @@
+package com.example.lhf1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lhf1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Lhf1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/lhf1/Lhf1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.lhf1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lhf1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
